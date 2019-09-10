e98d58fd1178d19db1a52a5c1f1b39e0ba3e8d20
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-10 18:50:56 +0000

    Fixing output exercise 12

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -3,10 +3,6 @@ def fizzbuzz():
 
     for i in range(1, 100):
 
-        if i == 99:
-            print("Fizz Buzz")
-            break
-
         if (i % 3) == 0 and (i % 5) == 0:
             print("Fizz Buzz ", end="")
         elif (i % 3) == 0:
