e8da5ded37073a2f44529cb951d1c53df484b83f
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-12 17:32:06 +0000

    Fixing requirements for exercise 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
 
-argc = len(sys.argv) - 1
-i = 1
+    import sys
 
-if argc == 0:
-    print("0 arguments.")
-elif argc == 1:
-    print("1 argument:")
-else:
-    print("{} arguments:".format(argc))
+    argc = len(sys.argv) - 1
+    i = 1
 
-for arg in sys.argv[1:]:
-    print("{}: {}".format(i, arg))
-    i += 1
+    if argc == 0:
+        print("0 arguments.")
+    elif argc == 1:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(argc))
+
+    for arg in sys.argv[1:]:
+        print("{}: {}".format(i, arg))
+        i += 1
