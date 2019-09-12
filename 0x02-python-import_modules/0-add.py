6339e1f5ceb25374e267500cd862dc47d1fead10
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-12 17:22:07 +0000

    Fixing requirements

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-if __name__ = "__main__":
+if __name__ == "__main__":
     from add_0 import add
 
     a = 1
     b = 2
-    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{0:d} + {1:d} = {2:d}".format(a, b, add(a, b)))
