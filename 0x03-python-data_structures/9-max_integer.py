b65e4f5b3bbedb133071615eb820035ab37ff53c
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-17 19:04:53 +0000

    Add exercise 9

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def max_integer(my_list=[]):
+    len_list = len(my_list)
+    result = 0
+    if len_list == 0:
+        return None
+    for i in my_list:
+        if i > result:
+            result = i
+    return result
