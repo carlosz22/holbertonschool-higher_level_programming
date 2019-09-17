d1d06c586fa44c6ab26b6058982e096d68e227b7
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-17 19:07:11 +0000

    Fix variable initialization in exercise 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
     len_list = len(my_list)
-    result = 0
+    result = my_list[0]
     if len_list == 0:
         return None
     for i in my_list:
