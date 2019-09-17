38bfa5a5dedc3f5395664ad5e194ddcb9547e791
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-17 21:24:23 +0000

    Fixing exercise 13

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -10,21 +10,24 @@ int is_palindrome(listint_t **head)
 	int nodes = 0;
 	int i = 0;
 	int j = 0;
-	int list[1000];
+	int list[9000];
+	listint_t *tmp;
 
-	if (*head == NULL)
+	tmp = *head;
+
+	if (head == NULL || *head == NULL || (*head)->next == NULL )
 		return (1);
 
-	while (*head != NULL)
+	while (tmp != NULL)
 	{
 		list[nodes] = (*head)->n;
-		*head = (*head)->next;
+		tmp = tmp->next;
 		nodes++;
 	}
 
 	i = nodes - 1;
 
-	while (i >= 0)
+	while (i >= 0 && j <= i)
 	{
 		if (list[i] != list[j])
 			return (0);
