469977e815b2cafbdde0448d710e3064eec83102
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-17 18:20:58 +0000

    Fix shebang in exercise 2-6

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     list_len = len(my_list)
     my_new_list = my_list[:]
--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,10 +1,11 @@
+#!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
     height = len(matrix)
     width = len(matrix[0])
     for i in range(height):
         j = 0
         for j in range(width):
-            print("{}".format(matrix[i][j]), end='')
+            print("{:d}".format(matrix[i][j]), end='')
             if j < (width - 1):
                 print(" ", end='')
         print()
