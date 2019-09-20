d322620cbea7a02d969cac605d1f7c6b5d97a910
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-20 01:18:36 +0000

    Fixing exercise 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or a_dictionary == {}:
         return None
     result = list(a_dictionary.keys())[0]
     first = a_dictionary[result]
     for key, value in a_dictionary.items():
         if value > first:
             result = key
+            first = value
+    if first <= 0:
+        return None   
     return result
