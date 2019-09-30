5dccb47db36dd44b52149f490e58f46e79dd4e75
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-30 18:17:33 +0000

    Add exercise 8

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+
+
+def safe_function(fct, *args):
+    try:
+        result = fct(args[0], args[1])
+    except Exception as err:
+        print("Exception: {}".format(err), file=sys.stderr)
+        return None
+    return result
