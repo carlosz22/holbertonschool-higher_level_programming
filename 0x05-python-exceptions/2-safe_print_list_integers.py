346b8213c00776714d182ff71593fd475b2576f0
Carlos Zuluaga <carlosez9@hotmail.com>
2019-09-30 17:05:23 +0000

    Fix range in for loop

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
     int_printed = 0
-    for i in range(x + 1):
+    for i in range(x):
         try:
             print("{:d}".format(my_list[i]), end='')
             int_printed += 1
