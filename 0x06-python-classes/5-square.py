12c2b85e063cc605bdb104e24537335d1916616c
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-01 21:07:06 +0000

    Fix output for exercise 5

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -46,5 +46,8 @@ class Square:
     def my_print(self):
         """
         """
-        for i in range(self.__size + 1):
+        for i in range(self.__size):
             print('#' * self.__size)
+
+        if self.__size == 0:
+            print()
