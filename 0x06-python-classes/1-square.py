482af9fef0877bfbeb0521f3d8a04ef060987678
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-01 20:48:01 +0000

    Add exercise 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+"""
+
+
+class Square:
+    """Creates a new object of class Square
+    """
+    def __init__(self, size):
+        """Constructor for the class Square
+
+        Args:
+            size (int): Square's size
+
+        Attributes:
+            size (int): Square's size
+
+        """
+        self.__size = size
