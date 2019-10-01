10864564bace378f0e31b45c515c43dade61b284
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-01 22:37:18 +0000

    Fixing if statement in position settler

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -48,9 +48,9 @@ class Square:
     def position(self, position):
         """A
         """
-        if type(self.__position) is not tuple or len(self.__position) != 2 or
-        type(self.__position[0]) is not int or type(self.__position[1]) is not int
-        or self. __position[0] < 0 or self.__position[1] < 0:
+        if type(position) is not tuple or len(position) != 2 or
+        type(position[0]) is not int or type(position[1]) is not int
+        or position[0] < 0 or position[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
 
         self.__position = position
