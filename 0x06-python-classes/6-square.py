ecd53eb65324af47b8dbf51388a399822ae58dfb
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-02 01:46:24 +0000

    Fix print function in exercise 6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -78,7 +78,7 @@ class Square:
             return
 
         if self.__position[0] > 0 and self.__position[1] > 0:
-            print("\n" * self.__position[1])
+            print("\n" * self.__position[1], end='')
 
         for i in range(self.__size):
             print(' ' * self.__position[0], end='')
