99f563b5ea076539ffd5a39b5a17382c44f85250
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-02 01:41:48 +0000

    Fix if statement on multiple lines

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -22,9 +22,9 @@ class Square:
             raise ValueError('size must be >= 0')
         self.__size = size
 
-        if type(position) is not tuple or len(position) != 2 or
-        type(position[0]) is not int or type(position[1]) is not int
-        or position[0] < 0 or position[1] < 0:
+        if type(position) is not tuple or len(position) != 2 or\
+           type(position[0]) is not int or type(position[1]) is not int or\
+           position[0] < 0 or position[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
         self.__position = position
 
@@ -36,16 +36,16 @@ class Square:
         return self.__size
 
     @size.setter
-    def size(self, size):
+    def size(self, value):
         """A
 
         """
-        if type(size) is not int:
+        if type(value) is not int:
             raise TypeError('size must be an integer')
-        if size < 0:
+        if value < 0:
             raise ValueError('size must be >= 0')
 
-        self.__size = size
+        self.__size = value
 
     @property
     def position(self):
@@ -57,9 +57,9 @@ class Square:
     def position(self, value):
         """A
         """
-        if type(value) is not tuple or len(value) != 2 or
-        type(value[0]) is not int or type(value[1]) is not int
-        or value[0] < 0 or value[1] < 0:
+        if type(value) is not tuple or len(value) != 2 or\
+           type(value[0]) is not int or type(value[1]) is not int or\
+           value[0] < 0 or value[1] < 0:
             raise TypeError('position must be a tuple of 2 positive integers')
 
         self.__position = value
