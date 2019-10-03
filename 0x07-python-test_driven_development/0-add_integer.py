9a76564e00e99a50193ec140bd811873308dbd2d
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-03 22:26:04 +0000

    Add exercise 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+"""
+
+def add_integer(a, b=98):
+    """
+    """
+    if isinstance(a, int) or isinstance(a, float):
+        a = int(a)
+    else:
+        raise TypeError('a must be an integer')
+
+    if isinstance(b, int) or isinstance(b, float):
+        b = int(b)
+    else:
+        raise TypeError('b must be an integer')
+
+    return a + b
---
+++ 0x07-python-test_driven_development/README.md
@@ -0,0 +1,3 @@
+# Read me
+
+This is the readme file for 0x07-python-test_driven_development
