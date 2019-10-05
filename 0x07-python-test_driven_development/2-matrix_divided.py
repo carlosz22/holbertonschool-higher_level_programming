a2526c711ff5a62b7d93a38e6c9fa7f69e672bd2
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-05 04:20:25 +0000

    Fixing pep8 style for exercise 1

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -13,21 +13,25 @@ def matrix_divided(matrix, div):
 
     try:
         if len(matrix) == 0:
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError('matrix must be a matrix (list of lists) of '
+                            'integers/floats')
     except TypeError:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError('matrix must be a matrix (list of lists) of '
+                        'integers/floats')
 
     new_matrix = []
 
     try:
         size = len(matrix[0])
     except TypeError:
-        raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+        raise TypeError('matrix must be a matrix (list of lists) of '
+                        'integers/floats')
 
     for i in range(len(matrix)):
 
         if not isinstance(matrix[i], list):
-            raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            raise TypeError('matrix must be a matrix (list of lists) of '
+                            'integers/floats')
         elif size != len(matrix[i]):
             raise TypeError('Each row of the matrix must have the same size')
 
@@ -37,8 +41,10 @@ def matrix_divided(matrix, div):
 
         for j in range(len(matrix[i])):
 
-            if not isinstance(matrix[i][j], int) and not isinstance(matrix[i][j], float):
-                raise TypeError('matrix must be a matrix (list of lists) of integers/floats')
+            if not isinstance(matrix[i][j], int) and \
+               not isinstance(matrix[i][j], float):
+                raise TypeError('matrix must be a matrix (list of lists) of '
+                                'integers/floats')
 
             result = matrix[i][j] / div
             result = round(result, 2)
