f309aa52deb6ea49c7ac6ed852c61f8cd35c3d31
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-05 04:49:31 +0000

    Adding test cases for exercise 3

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -11,5 +11,8 @@ def print_square(size):
     elif size < 0:
         raise ValueError('size must be >= 0')
 
+    if isinstance(size, float) and size < 0:
+        raise TypeError('size must be an integer')
+
     for i in range(size):
         print('#' * size)
