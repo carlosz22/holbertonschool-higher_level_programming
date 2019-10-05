f452062bfd78a2a72ad2719c9b031ed364c303e1
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-05 01:25:45 +0000

    Exercise 0 completed

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 """
+Module: `0-add_integer.py`
+
+This module contains the following implementation of functions or classes:
+
+add_integer
 """
 
 def add_integer(a, b=98):
-    """
+    """ Function: `add_integer` - Add two numbers (int or float)
+    Args: a: int or float, b: int or float
+    Return: On success, the integer of the addition of the two numbers
     """
     if isinstance(a, int) or isinstance(a, float):
         a = int(a)
