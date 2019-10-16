c8f3a5f183b9c7823b483b7e81718159d0edc317
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-16 04:03:38 +0000

    Add exercise 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module: `6-base_geometry.py`
+"""
+
+
+class BaseGeometry():
+    """class BaseGeometry
+    Class with area method, not implemented yet
+    """
+    def area(self):
+        """Calculates area"""
+        raise Exception('area() is not implemented')
