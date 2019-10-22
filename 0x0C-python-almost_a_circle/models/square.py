43b42563142b377d29470234fb43e4e5a9126a11
Carlos Zuluaga <carlosez9@hotmail.com>
2019-10-22 14:47:09 -0500

    Adding comments in square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 """
+Module: square
+Creates class Square
 """
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """
+    class Square
     """
 
     def __init__(self, size, x=0, y=0, id=None):
