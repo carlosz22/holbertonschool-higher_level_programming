a11f9894428f7307ca15871ce2faca26987c02e5
Carlos Zuluaga <carlosez9@hotmail.com>
2019-11-26 13:53:00 -0500

    Fix exercise 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- Creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states
 (id INT UNIQUE NOT NULL AUTO_INCREMENT,
 name VARCHAR(256) NOT NULL);
