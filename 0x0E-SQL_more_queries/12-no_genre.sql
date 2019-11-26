23640b4c6edc82bcc45643207f9bc062fdb00fd5
Carlos Zuluaga <carlosez9@hotmail.com>
2019-11-26 14:39:36 -0500

    Add exercise 12

---
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -0,0 +1,8 @@
+-- Lists of all shows and therr genre_id including NULLs
+SELECT T_S.title, T_S_G.genre_id
+FROM tv_show_genres AS T_S_G
+RIGHT JOIN tv_shows AS T_S
+ON T_S_G.show_id = T_S.id
+WHERE T_S_G.genre_id IS NULL
+ORDER BY T_S.title ASC,
+T_S_G.genre_id;
