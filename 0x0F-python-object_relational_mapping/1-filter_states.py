29a16d36a971297148a38b03d2a30b73228dc352
Carlos Zuluaga <carlosez9@hotmail.com>
2019-12-16 01:51:12 +0000

    Add exercise 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                          db=sys.argv[3])
+                         db=sys.argv[3])
     cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+    sql = "SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC"
     cursor.execute(sql)
     rows = cursor.fetchall()
     for row in rows:
