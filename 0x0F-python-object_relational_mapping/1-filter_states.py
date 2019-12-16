892f33fa73aac117d2b8e9d62f20e63ad0f1fa91
Carlos Zuluaga <carlosez9@hotmail.com>
2019-12-15 20:10:17 -0500

    Fix __main__ exercise 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -4,17 +4,18 @@
 import sys
 import MySQLdb
 
-if (len(sys.argv) == 4):
-    db = MySQLdb.connect(host='localhost',
-                         port=3306,
-                         user=sys.argv[1],
-                         passwd=sys.argv[2],
-                         db=sys.argv[3])
-    cursor = db.cursor()
-    sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
-    cursor.execute(sql)
-    rows = cursor.fetchall()
-    for row in rows:
-        print(row)
-    cursor.close()
-    db.close()
+if __name__ == "__main__":
+    if (len(sys.argv) == 4):
+        db = MySQLdb.connect(host='localhost',
+                             port=3306,
+                             user=sys.argv[1],
+                             passwd=sys.argv[2],
+                             db=sys.argv[3])
+        cursor = db.cursor()
+        sql = "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC"
+        cursor.execute(sql)
+        rows = cursor.fetchall()
+        for row in rows:
+            print(row)
+        cursor.close()
+        db.close()
