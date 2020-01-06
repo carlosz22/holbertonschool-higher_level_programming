bb86ae8c0c5f43d2092464c8abf83cafb984425f
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-06 13:38:49 -0500

    Fix parameter 2 in exercise 5

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # POST request with parameters
-curl -s --data "email=hr@holbertonschool.com&param2=I%20will%20always%20be%20here%20for%20PLD" "$1"
+curl -s --data "email=hr@holbertonschool.com&subject=I%20will%20always%20be%20here%20for%20PLD" "$1"
