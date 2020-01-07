36a36e47041a1a9cf0d3e96db6faa87c16acf82f
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-07 14:10:51 -0500

    FIx exercise 6

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -12,20 +12,17 @@ def find_peak(list_of_integers):
     if not list_of_integers:
         return None
 
-    start = 0
-    end = len(list_of_integers) - 1
-    middle = int(end / 2)
-    arr = list_of_integers[:]
+    middle = int((len(list_of_integers) - 1) / 2)
 
     if len(list_of_integers) == 1:
-        return arr[0]
+        return list_of_integers[0]
     elif len(list_of_integers) == 2:
-        if arr[0] > arr[1]:
-            return arr[0]
+        if list_of_integers[0] > list_of_integers[1]:
+            return list_of_integers[0]
         else:
-            return arr[1]
+            return list_of_integers[1]
 
-    if arr[middle] < arr[middle + 1]:
-        return find_peak(arr[middle + 1:])
+    if list_of_integers[middle] < list_of_integers[middle + 1]:
+        return find_peak(list_of_integers[middle + 1:])
     else:
-        return find_peak(arr[:middle + 1])
+        return find_peak(list_of_integers[:middle + 1])
