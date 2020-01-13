4aa92c158bd78a0e5a4f48c59e828ddc506e2a50
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-13 14:33:04 -0500

    Add guards to prevent execution if imported - ex 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """
-DIsplays the X-Request-Id header from an HTTP response
+Displays the X-Request-Id header from an HTTP response
 """
 import urllib.request
 import sys
 
-
-url = sys.argv[1]
-req = urllib.request.Request(url)
-with urllib.request.urlopen(req) as response:
-    data = response.read()
-    print(response.getheader('X-Request-Id'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    req = urllib.request.Request(url)
+    with urllib.request.urlopen(req) as response:
+        data = response.read()
+        print(response.getheader('X-Request-Id'))
