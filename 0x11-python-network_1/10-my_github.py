b9b300ca83c20e6d33467911fff81e42c647a88f
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-13 18:12:22 -0500

    Add exercise 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Get id
+"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    req = requests.get('https://api.github.com/user', auth=(user, passwd))
+    print(req.json().get('id'))
