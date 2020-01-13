52ce94c09b2d8766676efa662a30cab66ebe8f19
Carlos Zuluaga <carlosez9@hotmail.com>
2020-01-13 14:55:03 -0500

    Add exercise 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+Sends a POST request with a parameter
+"""
+import urllib.request
+import urllib.parse
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+    url_param = {'email': email}
+
+    data = urllib.parse.urlencode(url_param)
+    data = data.encode('utf8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        print(response.read().decode('utf8'))
