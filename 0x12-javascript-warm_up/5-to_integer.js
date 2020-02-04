92a41f9cee23848a721bdf86c59581da7b795502
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-04 17:00:28 -0500

    Feature: Add exercise 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,7 +4,7 @@ const process = require('process');
 const arg1 = process.argv[2];
 const arg1Parsed = parseInt(arg1);
 if (arg1Parsed) {
-  console.log('My number: ' + arg1);
+  console.log('My number: ' + arg1Parsed);
 } else {
   console.log('Not a number');
 }
