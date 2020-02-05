fe772bab71969a3adeb9e3f75157757fc0d26e62
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-04 21:21:36 -0500

    Feature: Add exercise 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const process = require('process');
+const arg1 = process.argv[2];
+const arg2 = process.argv[3];
+const arg1Parsed = parseInt(arg1);
+const arg2Parsed = parseInt(arg2);
+
+function add(a, b) {
+  console.log(a + b);
+}
+
+add(arg1Parsed, arg2Parsed);
