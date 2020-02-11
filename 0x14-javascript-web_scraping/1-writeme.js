aa884070a2fae59d530f45ac80ba415e3c30dcd0
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-11 12:23:03 -0500

    Feature: Add exercise 1

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+const fileDest = process.argv[2];
+const content = process.argv[3];
+
+fs.writeFile(fileDest, content, 'utf-8', function (err, data) {
+  if (err) {
+    console.log(err);
+  }
+});
