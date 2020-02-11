2dfa3a52d83b83203d1859de2b1ed23d3a62945f
Carlos Zuluaga <carlosez9@hotmail.com>
2020-02-10 21:12:14 -0500

    Feature: Add exercise 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    for (let i = 0; i < this.height; i++) {
+      if (c == null) {
+        console.log('X'.repeat(this.width));
+      } else {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+
+module.exports = Square;
